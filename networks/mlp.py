172f5153f6b64061f04beab203fc481d8d62fb67
uchikun2493 <um2493@gmail.com>
2019-12-25 18:04:23 +0900

    clean

--- networks/mlp.py
+++ networks/mlp.py
@@ -1,21 +1,7 @@
-# my default import modules
-import os
-import sys
-import numpy as np
-import matplotlib.pyplot as plt
-
-import json
-import math
-
 # pytorch
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-# import my funciton
-sys.path.append('../functions')
-import convert_dtype
-from convert_dtype import np_to_dataloder
 
 # **********************************************
 # mlp class
